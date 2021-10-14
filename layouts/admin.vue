750a8f51ebb4d60230e6c139523af85cfd6a1c01
Rodrigo <rodrigotilli@gmail.com>
2021-10-13 21:13:07 -0300

    layouts admin y default

---
+++ layouts/admin.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app :dark="$vuetify.theme.dark" :class="!$vuetify.theme.dark ? 'bgLight':''">
+    <base-nav-bar />
+    <v-main>
+      <v-container fluid >
+        <nuxt />
+      </v-container>
+    </v-main>
+    <base-footer />
+  </v-app>
+</template>
+
+<script>
+import BaseFooter from "~/components/ui/layouts/BaseFooter.vue";
+import BaseNavBar from "~/components/ui/layouts/BaseNavBar.vue";
+export default {
+  name: "admin",
+  components: { BaseNavBar, BaseFooter }
+};
+</script>
+<style>
+html {
+  overflow-y: auto !important;
+}
+.bgLight {
+  background-color: #ebecef !important;
+}
+</style>
