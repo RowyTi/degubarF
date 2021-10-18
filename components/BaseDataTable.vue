776d93d6493c840dfb1a4ba2578eef7aa0b360b0
Rodrigo <rodrigotilli@gmail.com>
2021-10-18 14:40:24 -0300

    Create BaseDataTable.vue
    componente data table

---
+++ components/BaseDataTable.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-data-table
+    :headers="headers"
+  
+  ></v-data-table>
+</template>
+
+<script>
+export default {
+    name: "BaseDataTable",
+    props: {
+        headers: {
+            type: Array,
+            default: () => [],
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
