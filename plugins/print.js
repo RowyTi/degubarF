eb4ab7e033a03710ef82fca8f0ede00145a6c2a3
Rodrigo <rodrigotilli@gmail.com>
2021-12-03 09:11:48 -0300

    Update print.js

--- plugins/print.js
+++ plugins/print.js
@@ -3,17 +3,17 @@ import VueHtmlToPaper from 'vue-html-to-paper';
 
 const options = {
   name: '_blank',
-  // specs: [
-  //   'fullscreen=no',
-  //   'titlebar=no',
-  //   'scrollbars=no'
-  // ],
+  specs: [
+    'fullscreen=no',
+    'titlebar=no',
+    'scrollbars=no'
+  ],
   styles: [
     'https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css'
   ],
-  // timeout: 1000, // default timeout before the print window appears
-  // autoClose: true, // if false, the window will not close after printing
-  // windowTitle: window.document.title, // override the window title
+  timeout: 500, // default timeout before the print window appears
+  autoClose: true, // if false, the window will not close after printing
+  windowTitle: window.document.title, // override the window title
 }
 
 Vue.use(VueHtmlToPaper, options);
